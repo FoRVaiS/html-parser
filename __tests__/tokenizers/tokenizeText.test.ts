13bbef05529cb23b8d254526d603f08e7d631a4f
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-07-09 05:20:30 -0700

    feat(tokenizers): tokenize words instead of entire strings
    Convert tokenizeText to tokenize text by words instead of the entire
    string which allows the flexibility to carefully delimit by a set of
    given delimiters.

--- __tests__/tokenizers/tokenizeText.test.ts
+++ __tests__/tokenizers/tokenizeText.test.ts
@@ -1,18 +1,52 @@
 import { TokenType } from '!types/index';
 import { tokenizeText as tokenize } from '!tokenizers/tokenizeText';
 
-it('should tokenize a text node', () => {
-  const input = 'This is a text node';
+it('should tokenize a single word', () => {
+  const input = 'Text';
 
-  const result = tokenize({
+  const result = tokenize()({
     cursor: 0,
     data: input,
   });
 
   const expected: typeof result = [{
     type: TokenType.TEXT,
-    processed: 19,
-    value: 'This is a text node',
+    processed: 4,
+    value: 'Text',
+  }];
+
+  expect(result).toStrictEqual(expected);
+});
+
+it('should tokenize a single word starting with a space', () => {
+  const input = ' Text';
+
+  const result = tokenize()({
+    cursor: 0,
+    data: input,
+  });
+
+  const expected: typeof result = [{
+    type: TokenType.TEXT,
+    processed: 5,
+    value: 'Text',
+  }];
+
+  expect(result).toStrictEqual(expected);
+});
+
+it('should tokenize a single word from a text node', () => {
+  const input = 'This is a text node';
+
+  const result = tokenize()({
+    cursor: 0,
+    data: input,
+  });
+
+  const expected: typeof result = [{
+    type: TokenType.TEXT,
+    processed: 4,
+    value: 'This',
   }];
 
   expect(result).toStrictEqual(expected);
@@ -21,66 +55,117 @@ it('should tokenize a text node', () => {
 it('should be able to tokenize at an offset', () => {
   const input = '<span>This is a text node</span>';
 
-  const result = tokenize({
+  const result = tokenize()({
     cursor: 6,
     data: input,
   });
 
   const expected: typeof result = [{
     type: TokenType.TEXT,
-    processed: 19,
-    value: 'This is a text node',
+    processed: 4,
+    value: 'This',
+  }];
+
+  expect(result).toStrictEqual(expected);
+});
+
+it('should accept a list of delimiters', () => {
+  const input = '<span class="container"></span>';
+
+  const result = tokenize(['='])({
+    cursor: 6,
+    data: input,
+  });
+
+  const expected: typeof result = [{
+    type: TokenType.TEXT,
+    processed: 5,
+    value: 'class',
+  }];
+
+  expect(result).toStrictEqual(expected);
+});
+
+it('should still delimit by whitespace even given a list of delimiters', () => {
+  const input = 'Text node';
+
+  const result = tokenize(['='])({
+    cursor: 0,
+    data: input,
+  });
+
+  const expected: typeof result = [{
+    type: TokenType.TEXT,
+    processed: 4,
+    value: 'Text',
   }];
 
   expect(result).toStrictEqual(expected);
 });
 
 it('should not prematurely interrupt tokenization when it comes across a \'<\'', () => {
-  const input = 'This is a text <node';
+  const input = 'Te<xt';
 
-  const result = tokenize({
+  const result = tokenize()({
     cursor: 0,
     data: input,
   });
 
   const expected: typeof result = [{
     type: TokenType.TEXT,
-    processed: 20,
-    value: 'This is a text <node',
+    processed: 5,
+    value: 'Te<xt',
   }];
 
   expect(result).toStrictEqual(expected);
 });
 
 it('should interrupt tokenization when it encounters an element', () => {
-  const input = 'This is a text <span></span>node';
+  const input = 'Text<span></span>node';
 
-  const result = tokenize({
+  const result = tokenize()({
     cursor: 0,
     data: input,
   });
 
   const expected: typeof result = [{
     type: TokenType.TEXT,
-    processed: 15,
-    value: 'This is a text',
+    processed: 4,
+    value: 'Text',
   }];
 
   expect(result).toStrictEqual(expected);
 });
 
 it('should correctly determine exactly which character is part of an element tag', () => {
-  const input = 'This is a text <<span></span>node';
+  const input = 'Text<<span></span>node';
 
-  const result = tokenize({
+  const result = tokenize()({
     cursor: 0,
     data: input,
   });
 
   const expected: typeof result = [{
     type: TokenType.TEXT,
-    processed: 16,
-    value: 'This is a text <',
+    processed: 5,
+    value: 'Text<',
+  }];
+
+  expect(result).toStrictEqual(expected);
+});
+
+it('should be able to tokenize words inside elements', () => {
+  const input = '<span class="container"></span>';
+
+  const result = tokenize()({
+    cursor: 1,
+    data: input,
+  });
+
+  const expected: typeof result = [{
+    type: TokenType.TEXT,
+    processed: 4,
+    value: 'span',
   }];
 
   expect(result).toStrictEqual(expected);
--- src/tokenizers/tokenizeText.ts
+++ src/tokenizers/tokenizeText.ts
@@ -1,20 +1,31 @@
 import { type Tokenizer, TokenType } from '!types/index';
 
 /** Tokenize a string of characters up until the first occurrence of an element or the end of string. */
-export const tokenizeText: Tokenizer = state => {
+export const tokenizeText: (delimiters?: Array<string>) => Tokenizer = (delimiters = []) => state => {
   // The index of the first occurrence of a closing bracket
   const endingBracketIndex = state.data.indexOf('>', state.cursor);
 
   // The index of the first occurrence of a opening bracket to the left of a closing bracket position
   const openingBracketIndex = state.data.lastIndexOf('<', endingBracketIndex);
 
-  // An element has been detected if both opening and closing positions both have an index larger than -1
-  const hasElement = openingBracketIndex > -1 && endingBracketIndex > -1;
-  
-  // Slice the text from the cursor's position to just before the opening bracket
-  const text = state.data.slice(state.cursor, hasElement
-    ? openingBracketIndex
-    : state.data.length);
+  // Find the first occurrence of each delimiter, filtering out any
+  // delimiter indices that could not be found
+  delimiters.push(' ');
+
+  const delimiterIndices = delimiters
+    .reduce((acc, delimiter) => {
+      const index = state.data.trimStart().indexOf(delimiter, state.cursor);
+      acc.push(index);
+
+      return acc;
+    }, [openingBracketIndex])
+    .filter(index => index > -1 && index >= state.cursor);
+
+  // Select the smallest valid index, infinity if indices list is empty
+  const sliceEndRange = Math.min(...delimiterIndices);
+
+  // Slice the text from the cursor's position to just before the delimiter
+  const text = state.data.slice(state.cursor, sliceEndRange);
 
   return [{
     type: TokenType.TEXT,
